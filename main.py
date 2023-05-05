971f5ff49c7cb2ab609d2971c3a7a854e7b0163e
Hellen <109177631+hellenilda@users.noreply.github.com>
2023-05-04 21:25:32 -0300

    Label

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import tkinter as tk
 from tkinter import *
 
+largura, altura = 650, 400
+
 tela = tk.Tk()
+tela.geometry(f'{largura}x{altura}')
+tela.title('SATA')
+
+Label(tela,text='PC 1').place(x=42,y=altura/2)
+Label(tela,text='PC 2').place(x=largura/2,y=altura/2)
+Label(tela,text='PC 3').place(x=325,y=altura/2)
 
 tela.mainloop()
