9147660a63a5181927c95ff5f8d3266702e68a29
hellenilda <hellen95limaaraujo@gmail.com>
2023-05-04 21:34:55 -0300

    Ajuste das Labels

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ tela = tk.Tk()
 tela.geometry(f'{largura}x{altura}')
 tela.title('SATA')
 
-Label(tela,text='PC 1').place(x=42,y=altura/2)
+Label(tela,text='PC 1').place(x=30,y=altura/2)
 Label(tela,text='PC 2').place(x=largura/2,y=altura/2)
-Label(tela,text='PC 3').place(x=325,y=altura/2)
+Label(tela,text='PC 3').place(x=580,y=altura/2)
 
 tela.mainloop()
