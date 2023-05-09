07c22695340d591a219fe5b09c0fbcb58a559c7d
hellenilda <hellen95limaaraujo@gmail.com>
2023-05-09 15:06:26 -0300

    PySide6

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from PySide6.QtWidgets import QApplication, QWidget
+from PySide6.QtWidgets import QApplication, QWidget, QLabel
 import sys
 
 app = QApplication(sys.argv)
 
 janela = QWidget()
 janela.show()
-app.exec_()
+app.exec()
