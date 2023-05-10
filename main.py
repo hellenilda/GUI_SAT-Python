a881a7e3d9d1c6e6f0dea6335f9958bcbad5b57f
hellenilda <hellen95limaaraujo@gmail.com>
2023-05-10 17:33:43 -0300

    QFrame

--- main.py
+++ main.py
@@ -23,81 +23,43 @@ class MainWindow(QMainWindow):
         ''')
         frameLateral.setGeometry(0,40,60,460)
 
-        btnPC1 = QPushButton('PC 1')
-        btnPC2 = QPushButton('PC 2')
-        btnPC3 = QPushButton('PC 3')
-        btnPC4 = QPushButton('PC 4')
-        btnPC5 = QPushButton('PC 5')
-        btnPC6 = QPushButton('PC 6')
-
-        mainLayout = QVBoxLayout()
-        widget = QWidget()
-
-        widget.setLayout(mainLayout)
-        self.setCentralWidget(widget)
-
-
-        btnPC1.setStyleSheet('''
-        background-color: white;
-        border: 5px black;
+        frame1 = QFrame(self)
+        frame1.setStyleSheet('''
+        background-color: gray;
         border-radius: 10px;
-        height: 30px;
         ''')
+        frame1.setGeometry(80,60,110,100)
 
-        btnPC2.setStyleSheet('''
-        background-color: white;
-        border: 5px black;
-        border-radius: 10px;
-        height: 30px;
-        ''')
+        btnPC1 = QPushButton(self)
+        btnPC1.setText('PC 1')
+        btnPC1.setGeometry(100,200,60,30)
 
-        btnPC3.setStyleSheet('''
-        background-color: white;
-        border: 5px black;
-        border-radius: 10px;
-        height: 30px;
-        ''')
+        # btnPC1.setStyleSheet('''
+        # background-color: white;
+        # border: 5px black;
+        # border-radius: 10px;
+        # height: 30px;
+        # ''')
 
-        btnPC4.setStyleSheet('''
-        background-color: white;
-        border: 5px black;
-        border-radius: 10px;
-        height: 30px;
-        ''')
+        # def PC1_clicado():
+        #     print('PC 1')
+        # def PC2_clicado():
+        #     print('PC 2')
+        # def PC3_clicado():
+        #     print('PC 3')
+        # def PC4_clicado():
+        #     print('PC 4')
+        # def PC5_clicado():
+        #     print('PC 5')
+        # def PC6_clicado():
+        #     print('PC 6')
 
-        btnPC5.setStyleSheet('''
-        background-color: white;
-        border: 5px black;
-        border-radius: 10px;
-        height: 30px;
-        ''')
-
-        btnPC6.setStyleSheet('''
-        background-color: white;
-        border: 5px black;
-        border-radius: 10px;
-        height: 30px;
-        ''')
-
-        def PC1_clicado():
-            print('PC 1')
-        def PC2_clicado():
-            print('PC 2')
-        def PC3_clicado():
-            print('PC 3')
-        def PC4_clicado():
-            print('PC 4')
-        def PC5_clicado():
-            print('PC 5')
-        def PC6_clicado():
-            print('PC 6')
-
-        btnPC1.clicked.connect(PC1_clicado)
-        btnPC2.clicked.connect(PC2_clicado)
-        btnPC3.clicked.connect(PC3_clicado)
-        btnPC4.clicked.connect(PC4_clicado)
-        btnPC5.clicked.connect(PC5_clicado)
-        btnPC6.clicked.connect(PC6_clicado)
+        # btnPC1.clicked.connect(PC1_clicado)
+        # btnPC2.clicked.connect(PC2_clicado)
+        # btnPC3.clicked.connect(PC3_clicado)
+        # btnPC4.clicked.connect(PC4_clicado)
+        # btnPC5.clicked.connect(PC5_clicado)
+        # btnPC6.clicked.connect(PC6_clicado)
 
 
 app = QApplication(sys.argv)
